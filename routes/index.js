623e2dc1215aa4f08ecde3162ccec8434c112267
Rohan1786 <rp9522203@gmail.com>
2024-06-24 19:05:41 +0530

    cookies manipulation

--- routes/index.js
+++ routes/index.js
@@ -4,42 +4,58 @@ var mongo=require('./users');
 const session = require('express-session');
 
 /* GET home page. */
-router.get('/create',async function(req, res, next) {
-const create=await mongo.create({
-    username:"rohan",
-  password:"123Rohan"
-})
+// router.get('/create',async function(req, res, next) {
+// const create=await mongo.create({
+//     username:"rohan",
+//   password:"123Rohan"
+// })
 
 
- res.send(create)
-});
-router.get('/delete',async function(req,res){
-const destro=await mongo.deleteMany({});
-res.send(destro);
-})
-router.get('/find',async function(req,res){
-  const findo=await mongo.find();
-  res.send(findo);
-  })
+//  res.send(create)
+// });
+// router.get('/delete',async function(req,res){
+// const destro=await mongo.deleteMany({});
+// res.send(destro);
+// })
+// router.get('/find',async function(req,res){
+//   const findo=await mongo.find();
+//   res.send(findo);
+//   })
   
-  router.get('/session',(req,res)=>{
-    const sess=req.session.ban=true;
-    console.log(sess)
-    res.send("hello hayhay")
+//   router.get('/session',(req,res)=>{
+//     const sess=req.session.ban=true;
+//     console.log(sess)
+//     res.send("hello hayhay")
 
-  })
-  router.get('/ban',(req,res)=>{
+//   })
+//   router.get('/ban',(req,res)=>{
   
-    console.log("hei")
-    if(req.session.ban==true){
-      res.send("your banned")
-    }
-    else{
+//     console.log("hei")
+//     if(req.session.ban==true){
+//       res.send("your banned")
+//     }
+//     else{
       
-    }
+//     }
    
 
+  // })
+
+  router.get('/',function(req,res){
+    res.cookie("age",21);
+
+    res.send("ban gayi cokie")
   })
-  
+
+   router.get('/read',function(req,res){
+    console.log(req.cookies)
+
+    res.send("Read ho gayi")
+  })
+  router.get('/delete',function(req,res){
+    console.log(res.clearCookie)
+
+    res.send("deleted cookies")
+  })
 
 module.exports = router;
